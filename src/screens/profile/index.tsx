663f66d16fbdf851bceb5039c66adfb6b6afc325
virendra.yadav <virendra.yadav@creativewebo.in>
2021-03-06 18:21:45 +0530

    changes done for api binding of user

--- src/screens/profile/index.tsx
+++ src/screens/profile/index.tsx
@@ -5,10 +5,11 @@ import { theme } from '~/styles'
 import { styles } from './styles'
 import { apiRequest } from '../../api-request'
 
+import { User } from '~/types'
 
 
 export const ProfileScreen: FC<RouteComponentProps> = observer(({ navigate }) => {
-  const [user,setUser] = useState(null)
+  const [user,setUser] = useState<User | undefined>(undefined)
   const [id,setID] = useState('')
   const [loading,setLoading] = useState(false)
   const [error, setError] = useState('')
@@ -36,11 +37,66 @@ export const ProfileScreen: FC<RouteComponentProps> = observer(({ navigate }) =>
     .finally(() => setLoading(false))
   }
 
-  
-
+  const CONTACT_ITEMS: Array<{
+    key: keyof Required<User>['contacts']
+    link?: boolean
+  }> = [
+    { key: 'phone' },
+    { key: 'email' },
+    { key: 'website', link: true },
+    { key: 'company' },
+    { key: 'position' },
+    { key: 'location' },
+  ]
+  const SOCIAL_LINKS: Record<keyof User['identities'], any> = {
+    telegram: 'https://t.me',
+    twitter: 'https://twitter.com',
+    // facebook: 'https://facebook.com',
+    facebook: 'fb://page/',
+    instagram: 'https://instagram.com',
+    linkedin: 'https://linkedin.com/in',
+    pinterest: 'https://pinterest.com',
+    twitch: 'https://twitch.tv',
+    youtube: 'https://youtube.com/c',
+    spotify: 'https://open.spotify.com/user',
+    snapchat: 'https://snapchat.com/add',
+    tiktok: 'https://vm.tiktok.com/',
+    paypal: 'https://paypal.me',
+    whatsapp: 'https://wa.me',
+    appleMusic: 'https://music.apple.com',
+    googlepay: 'https://pay.google.com?phone=',
+    phonepe:'upi://pay?pa=',
+    paytm:'paytmmp://',
+    freelancer:'https://www.freelancer.in/u/',
+    calendly:'https://calendly.com/',
+  }
   return (
     <main css={[styles.container]}>
-    <section css={styles.content}></section></main>
+      <section css={styles.content}>
+      {CONTACT_ITEMS.filter((item) => Boolean(user?.contacts?.[item.key])).map((item) => (
+        <div>
+          <label>{item.key}</label><br/>
+          <label>{ user!.contacts![item.key]}</label><br/><br/>
+        </div>
+        
+    ))}
+
+
+      {
+      Object.entries(user?.identities || {})
+        .filter(([_, value]) => Boolean(value))
+          .map(([type, value]) => (
+            user?.hideIdentities[type] ? null : 
+            // console.log(SOCIAL_LINKS[type]+'/'+value,type)
+            <div>
+              <label>{type}</label><br />
+              <a href={SOCIAL_LINKS[type]+'/'+value}>{type}</a><br /><br />
+            </div>
+          )
+        )
+      }
+      </section>
+    </main>
     );
 })
 
--- src/types.ts
+++ src/types.ts
@@ -16,3 +16,75 @@ export interface PaymentFormData {
   zipCode: string
   phone: string
 }
+
+/** Represents user model. */
+export interface User {
+  _id?: string
+  firebaseId?: string
+
+  role?: 'user' | 'admin'
+  paymentCompleted?: boolean
+  shareCount : number,
+  tapsCount: number,
+  isVerified: boolean,
+  cardStatus:boolean,
+  contacts: {
+    name?: string
+    bio?: string
+
+    phone?: string
+    email?: string
+    website?: string
+
+    location?: string
+    company?: string
+    position?: string
+    avatarUrl?: any
+    coverUrl?: any
+  }
+
+  identities: UserIdentities
+  hideIdentities:hideIdentities
+  createdAt?: number
+  updatedAt?: number
+  //cardStatus?:boolean
+}
+
+export interface UserIdentities {
+  instagram: string
+  snapchat: string
+  twitter: string
+  facebook: string
+  linkedin: string
+  youtube: string
+  tiktok: string
+  spotify: string
+  appleMusic: string
+  paypal: string
+  whatsapp: string
+  twitch: string
+  telegram: string
+  pinterest: string
+  googlepay:string
+  phonepe:string,
+  paytm:string,
+  freelancer:string,
+  calendly:string,
+}
+export interface hideIdentities {
+  instagram: boolean
+  snapchat: boolean
+  twitter: boolean
+  facebook: boolean
+  linkedin: boolean
+  youtube: boolean
+  tiktok: boolean
+  spotify: boolean
+  appleMusic: boolean
+  paypal: boolean
+  whatsapp: boolean
+  twitch: boolean
+  telegram: boolean
+  pinterest: boolean
+ 
+}
