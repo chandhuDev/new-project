cd1494334a2f492459cd07bdaa8f72675edbd378
abhi_cw <abhishek.salvi@creativewebo.in>
2022-02-09 20:51:22 +0530

    live api added

--- src/config.ts
+++ src/config.ts
@@ -1,12 +1,12 @@
-// export const API_URL =
-//   process.env.NODE_ENV === 'production'
-//     ? 'https://api.onetaphello.com'
-//     : 'https://api.onetaphello.com'
-
 export const API_URL =
   process.env.NODE_ENV === 'production'
-    ? 'https://oth-api-test.onetaphello.com'
-    : 'https://oth-api-test.onetaphello.com'
+    ? 'https://api.onetaphello.com'
+    : 'https://api.onetaphello.com'
+
+// export const API_URL =
+//   process.env.NODE_ENV === 'production'
+//     ? 'https://oth-api-test.onetaphello.com'
+//     : 'https://oth-api-test.onetaphello.com'
 
 export const STRIPE_PUBLISHABLE_KEY =
   process.env.NODE_ENV === 'production'
