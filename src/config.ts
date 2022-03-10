60876b47d20a1059f837be2042e1aa6ed05d8bb7
abhi_cw <abhishek.salvi@creativewebo.in>
2022-03-10 11:45:56 +0530

    Revised code

--- src/config.ts
+++ src/config.ts
@@ -3,6 +3,7 @@
 //     ? 'https://api.onetaphello.com'
 //     : 'https://api.onetaphello.com'
 
+
 export const API_URL =
   process.env.NODE_ENV === 'production'
     ? 'https://oth-api-test.onetaphello.com'
